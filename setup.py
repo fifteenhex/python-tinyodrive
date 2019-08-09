fe7bb3537d78cb57b22cc30890652b07abdacd3e
Daniel Palmer <daniel@0x0f.com>
2019-08-10 05:28:55 +0900

    Project skeleton

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='tinyodrive',
+      version='0.1',
+      author='Daniel Palmer',
+      author_email='daniel@0x0f.com',
+      license='GPLv3',
+      packages=['tinyodrive'],
+      zip_safe=False)
---
+++ tinyodrive/__init__.py
@@ -0,0 +1,1 @@
+from tinyodrive import *
